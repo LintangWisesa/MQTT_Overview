06bd38914d41572df47a8c2b57686bc8d875e52f
lintangwisesa <lintangwisesa@ymail.com>
2019-11-11 11:48:01 +0700

    hbmqtt sub message

--- MQTT_Python/brokerApp_getMessage.py
+++ MQTT_Python/brokerApp_getMessage.py
@@ -3,7 +3,9 @@ import asyncio
 import os
 
 # pip install hbmqtt
-from hbmqtt.broker import Broker
+from hbmqtt.broker import Broker   
+from hbmqtt.client import MQTTClient, ClientException
+from hbmqtt.mqtt.constants import QOS_1, QOS_2
 
 logger = logging.getLogger(__name__)
 
@@ -40,9 +42,36 @@ def test_coro():
     #yield from asyncio.sleep(5)
     #yield from broker.shutdown()
 
+@asyncio.coroutine
+def uptime_coro():
+    C = MQTTClient()
+    yield from C.connect('mqtt://localhost/')
+    # Subscribe to '$SYS/broker/uptime' with QOS=1
+    yield from C.subscribe([
+        ('LINTANGtopic/test', QOS_1),
+        # ('$SYS/broker/load/#', QOS_2),
+    ])
+    logger.info("Subscribed")
+    try:
+        for i in range(1, 100):
+            message = yield from C.deliver_message()
+            packet = message.publish_packet
+            # print("%d: %s => %s" % (i, packet.variable_header.topic_name, str(packet.payload.data)))
+            # print(packet.payload.data)
+            # print(str(packet.payload.data))
+            print(packet.payload.data.decode("utf-8"))
+        
+        # yield from C.unsubscribe(['$SYS/broker/uptime', '$SYS/broker/load/#'])
+        # logger.info("UnSubscribed")
+        # yield from C.disconnect()
+    
+    except ClientException as ce:
+        logger.error("Client exception: %s" % ce)
+
 if __name__ == '__main__':
     formatter = "[%(asctime)s] :: %(levelname)s :: %(name)s :: %(message)s"
     #formatter = "%(asctime)s :: %(levelname)s :: %(message)s"
     logging.basicConfig(level=logging.INFO, format=formatter)
     asyncio.get_event_loop().run_until_complete(test_coro())
+    asyncio.get_event_loop().run_until_complete(uptime_coro())
     asyncio.get_event_loop().run_forever()
